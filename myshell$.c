90dffefe8c252c51a678b60048f72a8e401ac5f2
sharadshelke <142532716+sharadshelke@users.noreply.github.com>
2024-09-26 22:57:56 +0530

    myshell$.c

---
+++ myshell$.c
@@ -0,0 +1,60 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <fcntl.h> 
+ 
+void typeline(char *option, char *filename) { 
+    FILE *file = fopen(filename, "r"); 
+    if (file == NULL) { 
+        printf("File %s not found.\n", filename); 
+        return; 
+    } 
+ 
+    char line[1000]; 
+    int n; 
+ 
+    if (strcmp(option, "-a") == 0) { 
+        while (fgets(line, sizeof(line), file)) { 
+            printf("%s", line); 
+        } 
+    } else if (option[0] == '+' && isdigit(option[1])) { 
+        n = atoi(option + 1); 
+        for (int i = 0; i < n; i++) { 
+            if (fgets(line, sizeof(line), file)) { 
+                printf("%s", line); 
+            } 
+        } 
+    } 
+    fclose(file); 
+} 
+ 
+int main() { 
+    char command[100], *args[10]; 
+    while (1) { 
+printf("\nmyshell$ "); 
+fgets(command, 100, stdin); 
+command[strlen(command) - 1] = '\0';  // Remove newline 
+char *token = strtok(command, " "); 
+int i = 0; 
+while (token != NULL) { 
+args[i++] = token; 
+token = strtok(NULL, " "); 
+} 
+args[i] = NULL; 
+if (strcmp(args[0], "typeline") == 0) { 
+typeline(args[1], args[2]); 
+} else if (strcmp(args[0], "exit") == 0) { 
+exit(0); 
+} else { 
+int pid = fork(); 
+if (pid == 0) { 
+execvp(args[0], args); 
+exit(0); 
+} else { 
+wait(NULL); 
+} 
+} 
+} 
+return 0; 
+}
